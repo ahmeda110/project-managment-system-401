328f0663d4bbcfa975490f0e32484ffe1fefa41e
Saim-Shahzad <113881352+Saim-Shahzad@users.noreply.github.com>
2024-03-26 21:33:54 -0600

    added api for updating user name

--- client/src/components/SideBar/Sidebar.jsx
+++ client/src/components/SideBar/Sidebar.jsx
@@ -1,26 +1,15 @@
 import React, { useState, useEffect } from "react";
-
-import { RiHome2Fill } from "react-icons/ri";
 import { RiAccountBoxFill } from "react-icons/ri";
 import { FaSignOutAlt } from "react-icons/fa";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-
 import { PiProjectorScreenFill } from "react-icons/pi";
 import { FaCalendar } from "react-icons/fa";
 import { FaBorderAll } from "react-icons/fa6";
-
-import axios from "axios";
-
 import "../../assets/styles/Sidebar.css";
 
 function Sidebar({ activeTab, setActiveTab, activeSubTab, setActiveSubTab }) {
-  const navigate = useNavigate();
-  const { user, isAuthenticated, logout } = useAuth0();
-  const [openCategory, setOpenCategory] = useState(null);
-  const [userName, setUserName] = useState(localStorage.getItem("userName") || "");
-  const [memberId, setMemberId] = useState("");
-
   const navigationItems = {
     "All Projects": {
       icon: <PiProjectorScreenFill size={18} />,
@@ -36,12 +25,66 @@ function Sidebar({ activeTab, setActiveTab, activeSubTab, setActiveSubTab }) {
     Account: { icon: <RiAccountBoxFill size={20} />, link: "/account" },
   };
 
-  const handleItemClick = (itemName, itemLink) => {
-    if (itemName === "All Projects" && openCategory !== itemName) {
-      setOpenCategory(itemName);
-    } else {
-      setOpenCategory(null);
+  const navigate = useNavigate();
+  const { user, isAuthenticated, logout } = useAuth0();
+  const [userName, setUserName] = useState("");
+  const [memberId, setMemberId] = useState("");
+
+  useEffect(() => {
+    const fetchMemberData = async () => {
+      try {
+        const email = user.email;
+        const emailResponse = await axios.get(`/api/members/email/${email}`);
+        const memberId = emailResponse.data.memberId;
+        setMemberId(memberId);
+  
+        const memberNameResponse = await axios.get(`/api/members/${memberId}/name`);
+        const memberName = memberNameResponse.data.name;
+        setUserName(memberName);
+      } catch (error) {
+        console.error('Error fetching member data:', error);
+        if (error.response) {
+          await createNewMember();
+        }
+      }
+    };
+  
+    const createNewMember = async () => {
+      try {
+        await axios.post("/api/members", { email: user.email, name: user.name });
+        await fetchMemberData();
+      } catch (error) {
+        console.error('Error creating new member:', error);
+      }
+    };
+  
+    if (isAuthenticated && user) {
+      fetchMemberData();
     }
+  }, [isAuthenticated, user]);  
+
+  useEffect(() => {
+    const handleUsernameChange = async (event) => {
+      try {
+        if (user?.email && memberId) {
+          const updatedUserName = event.detail;
+          const response = await axios.put(`/api/members/${memberId}`, { name: updatedUserName });
+          console.log('Username updated');
+        }
+        setUserName(event.detail);
+      } catch (error) {
+        console.error('Error updating username:', error);
+      }
+    };
+
+    window.addEventListener('usernameUpdated', handleUsernameChange);
+
+    return () => {
+      window.removeEventListener('usernameUpdated', handleUsernameChange);
+    };
+  }, [user?.email, memberId]);
+
+  const handleItemClick = (itemName, itemLink) => {
     setActiveTab(itemName);
     setActiveSubTab(null);
     navigate(itemLink);
@@ -52,78 +95,15 @@ function Sidebar({ activeTab, setActiveTab, activeSubTab, setActiveSubTab }) {
     navigate(subItemLink);
   };
 
-  const updateUserNameFromStorage = (email) => {
-    const savedUserName = localStorage.getItem(email);
-    setUserName(savedUserName || email);
-  };
-
-  useEffect(() => {
-    const fetchMemberId = async () => {
-      try {
-        const response = await axios.get(`/api/members/email/${user.email}`);
-        console.log('Response:', response);
-        setMemberId(response.data.memberId);
-
-        const memberNameResponse = await axios.get(`/api/members/${response.data.memberId}/name`);
-        console.log('Member Name Response:', memberNameResponse);
-        const memberName = memberNameResponse.data.name;
-        console.log('Member Name:', memberName);
-      } catch (error) {
-        console.error('Error fetching member ID:', error);
-        if (error.response) {
-          // If email not found, create a new member
-          try {
-            const response = await axios.post("/api/members", { email: user.email, name: user.name });
-            const memberId = response.data.memberId;
-            setMemberId(memberId);
-
-            const memberNameResponse = await axios.get(`/api/members/${memberId}/name`);
-            const memberName = memberNameResponse.data.name;
-            setUserName(memberName);
-          } catch (error) {
-            console.error('Error creating new member:', error);
-          }
-        }
-      }
-    };
-
-    if (isAuthenticated && user) {
-      fetchMemberId();
-      localStorage.setItem("userEmail", user.email);
-      setUserName(user.email);
-    }
-  }, [isAuthenticated, user]);
-
-  useEffect(() => {
-    const handleUsernameChange = (event) => {
-      if (user?.email) {
-        localStorage.setItem(user.email, event.detail);
-      }
-      setUserName(event.detail);
-    };
-
-    window.addEventListener('usernameUpdated', handleUsernameChange);
-
-    return () => {
-      window.removeEventListener('usernameUpdated', handleUsernameChange);
-    };
-  }, [user?.email]);
-
-  useEffect(() => {
-    if (user) {
-      updateUserNameFromStorage(user.email);
-    }
-  }, [user]);
-
   return (
     <div className="sidebar-container">
       <div className="sidebar-header">
         <div className="profile">
-        {userName && userName.includes(" ") ? (
+          {userName && userName.includes(" ") ? (
             <>{userName.split(" ")[0][0]}{userName.split(" ")[1][0]}</>
           ) : (
             <>{userName?.[0]}</>
-        )}
+          )}
         </div>
         <div className="profile-name">{userName || "Loading..."}</div>
       </div>
